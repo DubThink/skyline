496c95302a91bda61b87f6e8fcf95bd8012a9f74
Diana Voyer <7999399+dvoyer@users.noreply.github.com>
2021-09-05 23:13:28 -0400

    update person with basic goal stuff

--- Person.gd
+++ Person.gd
@@ -5,9 +5,7 @@ extends PathFollow2D
 # var a = 2
 # var b = "text"
 
-var happiness_spread = 1
-onready var current_happiness: float = 0.5
-var home: BuildingInstance
+
 
 class_name Person
 
@@ -23,13 +21,73 @@ func _process(delta):
 
 var dir: int = 1;
 
+var happiness_spread = 1
+onready var current_happiness: float = 0.5
+
+enum GOAL{
+	NONE,
+	HOME,
+	WORK,
+	FOOD,
+	HEALTH
+}
+
+var home: BuildingInstance
+var work: BuildingInstance
+var is_child = false
+var current_goal
+var goal_timer
+var goal_pos = 0
+
 export (float, 1, 400) var walkspeed_pps
 
+# needs
+var hunger = 0
+var occupation = 0
+var medical = 0
+
+func tick_needs(delta):
+	hunger+=delta*0.003
+	# todo do boundsing
+	# todo the rest of the needs
+
+func decide_next_goal():
+	var time_of_day = SkyRenderManager.worldtime % SkyRenderManager.SECONDS_PER_DAY
+	current_goal = GOAL.NONE
+	var current_goal_strength = 0
+	if occupation > current_goal_strength:
+		current_goal = GOAL.WORK
+		current_goal_strength = occupation
+	if hunger > current_goal_strength:
+		current_goal = GOAL.FOOD
+		current_goal_strength = hunger
+	if time_of_day < 0.25 or time_of_day > .65:
+		current_goal = GOAL.HOME
+		current_goal_strength = 1.1
+	if medical > current_goal_strength:
+		current_goal = GOAL.HEALTH
+		current_goal_strength = medical
+	
+func find_next_goal():
+	match current_goal:
+		GOAL.HOME:
+			goal_pos = home.offset
+		GOAL.WORK:
+			goal_pos = work.offset
+		GOAL.FOOD:
+			goal_pos = 0 #TODO: find the things
+		GOAL.HEALTH:
+			goal_pos = 0 #TODO: find the things
+
 func walk_step(delta):
+	dir = -1 if offset > goal_pos else 1
 	var dist = dir*walkspeed_pps*delta
 	#print(offset)
 	set_offset(offset+dist)
-	
+	#if abs(goal_pos-offset)<10:
+		# go invis
+	#	goal_timer = new timer(60)
+
 func find_neighbors(dist):
 	var all_people = get_tree().get_nodes_in_group("people")
 	var neighbors = []
@@ -39,7 +97,7 @@ func find_neighbors(dist):
 		if (abs(i.offset - offset) < dist):
 			neighbors.append(i)
 	return neighbors
-	
+
 func update_happiness(dist):
 	var d_happiness = []
 	var neighbors = find_neighbors(dist)
